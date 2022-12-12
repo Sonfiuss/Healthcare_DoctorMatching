13bdc1ee6f90fa2f395cef51ac4051f68e5c544d
bannhatb <vovanban184@gmail.com>
2022-12-12 23:44:58 +0700

    signin signup savedata for doctor

--- doctormatching/accounts/urls.py
+++ doctormatching/accounts/urls.py
@@ -8,7 +8,10 @@ urlpatterns = [
 
     path('signup_patient', views.signup_patient, name="signup_patient"),
     path('sign_in_patient', views.sign_in_patient , name='sign_in_patient'),
-    
     path('savepdata/<str:patientusername>', views.savepdata , name='savepdata'),
+    
+    path('signup_doctor', views.signup_doctor , name="signup_doctor"),
+    path('sign_in_doctor', views.sign_in_doctor , name='sign_in_doctor'),
+    path('saveddata/<str:doctorusername>', views.saveddata , name='saveddata'),
     
 ]
