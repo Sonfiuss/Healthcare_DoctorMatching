d521ac1c3311ba2e36f722e721b3edde550f8b71
HoangSon <hthaison17@gmail.com>
2022-12-13 11:41:09 +0700

    delete disease_prediction

--- doctormatching/chats/models.py
+++ doctormatching/chats/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from main_app.models import consultation
+from main.models import consultation
 
 # Create your models here.
 
